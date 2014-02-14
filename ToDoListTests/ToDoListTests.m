af9cabb683c70a35b6fefbe39d23446279f099c9
Tushar Rawal <tushar.rawal@gmail.com>
2014-02-13 21:12:15 -0800

    Initial Commit

---
+++ ToDoListTests/ToDoListTests.m
@@ -0,0 +1,34 @@
+//
+//  ToDoListTests.m
+//  ToDoListTests
+//
+//  Created by Tushar Rawal on 2/13/14.
+//  Copyright (c) 2014 Tushar Rawal. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToDoListTests : XCTestCase
+
+@end
+
+@implementation ToDoListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
